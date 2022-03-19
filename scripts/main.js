d8a172e052e053d4d086cd956392d2a6027d9f58
Praisecodes <cmancoder@gmail.com>
2022-03-19 20:23:12 +0100

    Some little work

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+const desktop = document.getElementById("desktop");
+const NgPro = document.getElementById("NgPro");
+const OpenSource = document.getElementById("OpenSource");
+
 var typed = new Typed("#pcodes", {
     strings: [
         "A YOUNG DEVELOPER",
@@ -18,4 +22,26 @@ const ToHome = () => {
   document.getElementById('landing').scrollIntoView({
     behavior: 'smooth'
   });
+}
+
+window.onscroll = () => {
+  if(window.scrollY >= 100){
+    NgPro.style.display = "flex";
+    setTimeout(()=>{
+      NgPro.style.transform = "translate(0%)";
+      setTimeout(()=>{
+        OpenSource.style.display = "flex";
+        setTimeout(()=>{
+          OpenSource.style.transform = "translate(0%)";
+          setTimeout(() => {
+            desktop.style.display = "flex";
+            setTimeout(()=>{
+              desktop.style.transform = "translate(0%)";
+            },300);
+          },500);
+        },300)
+      },500);
+    },300)
+    
+  }
 }
